cdb5519d9578571cb74056ac204473df28264eb3
Anastasiya-Kl <aklurfeld@gmail.com>
2021-09-12 19:00:20 +0300

    init: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) !== 'string' ||  isNaN(sampleActivity) == true
+  || Number(sampleActivity) > 15 || Number(sampleActivity) <= 0) {
+    return false;
+  }else{
+    let result = Math.log(MODERN_ACTIVITY/sampleActivity) / (0.693 / HALF_LIFE_PERIOD);
+    result = Math.ceil(result);
+    return result;
+  }  
 }
